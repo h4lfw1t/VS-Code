9d0a45f384dc0b1cae1b1b781aa581b42e2a2ea4
h4lfw1t <graysonofgotham2112@gmail.com>
2024-01-25 21:13:01 -0500

    edited shit

---
+++ PHY 1112/ASSIGNMENTS/ASSIGNMENT 1/a1.2_300349204.py
@@ -0,0 +1,11 @@
+'''
+Filename:       a1.2_300349204.py
+Author:         Patrick Geraghty (pgera009@uottawa.ca)
+Date Created:   2024-01-25
+Date Modified:  2024-01-25
+Version:        1.0
+Description:    This program prints "Hello World" to the console where each word is on a separate line and indented with a tab.
+'''
+
+
+print("Hello", "World", sep="\n \t")
---
+++ PHY 1112/ASSIGNMENTS/ASSIGNMENT 1/a1.3_300349204.py
@@ -0,0 +1,10 @@
+'''
+Filename:       a1.3_300349204.py
+Author:         Patrick Geraghty (pgera009@uottawa.ca)
+Date Created:   2024-01-25
+Date Modified:  2024-01-25
+Version:        1.0
+Description:    Prints the user's input to the console.
+'''
+
+print(input("What would you like to print?: "))

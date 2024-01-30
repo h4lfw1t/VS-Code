6895705838828565d588017cbdbf45e82fa93cc6
Patrick Geraghty <144934696+h4lfw1t@users.noreply.github.com>
2024-01-30 11:23:01 -0500

    Converted 'LEC_8.py' code to a function

--- PHY 1112/LECTURES/LEC_8.py
+++ PHY 1112/LECTURES/LEC_8.py
@@ -1,10 +1,18 @@
-split_list = []
-master_list = []
-for i in range(12):
-    for j in range(12):
-        split_list.append((i+1) * (j+1))
+'''
+Filename: LEC_8.py
+Author: Patrick Geraghty
+Date Created: 2023-01-30
+Date Modified: 2023-01-30
+Description: Defines a function that prints a two-dimensional list containg the elements of a 12x12 multiplication table.
+'''
+def mult_table():
+    split_list = []
+    master_list = []
+    for i in range(12):
+        for j in range(12):
+            split_list.append((i+1) * (j+1))
 
-for i in range(0, len(split_list), 12):
-    master_list.append(split_list[i:i+12])
+    for i in range(0, len(split_list), 12):
+        master_list.append(split_list[i:i+12])
 
-print(master_list)
+    print(master_list)

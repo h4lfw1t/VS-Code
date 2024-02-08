cf970b622ea4af75ef6ec706334d3ae7195dfd85
Patrick Geraghty <144934696+h4lfw1t@users.noreply.github.com>
2024-02-08 09:52:33 -0500

    Refactor code for improved performance and readability

---
+++ PHY 1112/LECTURES/LEC_10_code.py
@@ -0,0 +1,44 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def snells_law(n1,n2,theta_i):
+    '''
+    (float, float, float) -> float
+    This function takes in the angle of incidence, and returns the angle of refraction.
+    Preconditions: 0 <= theta_i <= pi/2, n1, n2 > 0
+    '''
+    
+    return np.arcsin(n1/n2*np.sin(theta_i))
+
+def refraction_plot_glass(n1,n2):
+    '''
+    (float, float) -> None
+    This function takes in the angle of incidence, and plots the reflection coeffient as a function of the angle of incidence.
+    Preconditions: 0 <= theta_i <= pi/2, n1, n2 > 0
+    '''
+    # Law of reflection: angle of incidence = angle of reflection
+    # Snell's Law: n1*sin(theta1) = n2*sin(theta2)
+    # Reflection Coefficient: r = (cos(theta1) - n2/n1*cos(theta2))/(cos(theta1) + n2/n1*cos(theta2))
+    
+    x = np.linspace(0, np.pi/2, 100)
+    
+    plt.figure(1)
+    
+    plt.plot(x, (n1*np.cos(x) - n2*np.cos(snells_law(n1,n2,x)))/(n1*np.cos(x) + n2*np.cos(snells_law(n1,n2,x))), label = 'Reflection Coefficient')
+    
+    plt.hlines(0, 0, np.pi/2, colors = 'k', linestyles = 'dashed')
+    
+    plt.xlim(0, np.pi/2)
+    
+    xtick_values = [0, np.pi/6, np.pi/5, np.pi/4, np.pi/3, np.pi/2]
+    xtick_labels = ['0', r'$\frac{pi}{6}$', r'$\frac{pi}{5}$', r'$\frac{pi}{4}$', r'$\frac{pi}{3}$', '$\frac{pi}{2}$']
+    plt.xticks
+    
+    plt.xlabel('Angle of Incidence (radians)')
+    plt.ylabel('Reflection Coefficient')
+    plt.title('Reflection Coefficient as a Function of Angle of Incidence')
+    plt.legend()
+    
+    plt.grid()
+    
+    plt.show()

8e6404a60e1b22b5c88ef2140553b2950cf532e9
Patrick Geraghty <144934696+h4lfw1t@users.noreply.github.com>
2024-02-13 11:16:50 -0500

    Fix multiplication function for negative inputs and add test cases

--- PHY 1112/ASSIGNMENTS/ASSIGNMENT 5/a5_300349204.py
+++ PHY 1112/ASSIGNMENTS/ASSIGNMENT 5/a5_300349204.py
@@ -70,3 +70,44 @@ def fibber_loop(x):
     t1 = t.perf_counter()
     print(f"Loop time: {t1-t0}")
 
+
+
+# 2. A Quest for Quality
+
+# define function 'recursive_multiplication'
+def recursive_multiplication(a, b):
+    '''
+    (int, int) -> int
+    Returns the product of two integers using recursion.
+    Preconditions: a and b are integers.
+    '''
+    # define base case
+    if b == 0:                      # if b is 0, return 0
+        return 0
+    elif a == 0:                      # if a is 0, return 0
+        return 0
+    elif a < 0 and b < 0:             # if a and b are both negative, return positive product
+        return recursive_multiplication(-a, -b)
+    elif a < 0:                       # if a is negative, return negative product
+        return -recursive_multiplication(-a, b)
+    elif b < 0:                       # if b is negative, return negative product
+        return -recursive_multiplication(a, -b)
+    else:                           # if b is not 0, calculate product using recursion
+        return a + recursive_multiplication(a, b-1)
+
+# define simple loop for test cases
+def rec_mult_loop_output(a, b):
+    '''
+    (int, int) -> None
+    Returns the products of a range of two integers using recursion.
+    Preconditions: a and b are integers.
+    '''
+    # note that the range of a is equal to the range of b
+    for i in range(a, b):
+        for j in range(a, b):
+            print(f"{i} * {j} = {recursive_multiplication(i, j)}")
+
+
+
+# 3. Rigid Read-In
+
